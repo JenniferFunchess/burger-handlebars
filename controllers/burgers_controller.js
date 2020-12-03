496cd9348f9f7d72d3786df47ae5b5b000c436bb
Jennifer Funchess <jfunchess@ymail.com>
2020-12-02 22:22:40 -0600

    controllers setup complete

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = {
+      burgers: data,
+    };
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function (req, res) {
+  burger.create(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function (result) {
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+router.put("/:id", function (req, res) {
+  console.log(req.params.id);
+
+  burger.updateOne(req.params.id, function () {
+    res.redirect("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
